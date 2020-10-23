c58a3d989b8d43d54beb8fe01b27fa81d58c1e1d
MrAstorii <72743039+MrAstorii@users.noreply.github.com>
2020-10-23 11:08:11 +0200

    Pierwsze zajęcia

---
+++ Godot1/Node.gd
@@ -0,0 +1,15 @@
+extends Node
+
+var welcomeFirst = "Hello"
+var welcomeSecond = "Holla"
+var nameFirst = "John"
+var nameSecound = "Juan"
+var number = 10
+
+var names = ["Jan", "John", "Juan"]
+var welcomes = ["Cześć", "Hello", "Holla"]
+func _ready():
+	print(names[1] + " my name is " + welcomes[1])
+
+#func _process(delta):
+#	pass
